0f4cbdf7ce83d8d0bd85f6dde80c7e2a3dfac8f7
nisha.g <nisha.g@finacus.co.in>
2025-05-08 19:32:26 +0530

    made signup and login api , encrypted password

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const {validateSignUpData} = require('./utils/validation')
+const bcrypt = require('bcrypt')
 
 const app = express();
 const User = require('./models/user')
@@ -11,109 +13,159 @@ app.use(express.json())
 //sign up api
 
 
-app.post('/signup', async(req, res)=>{
+//for signup api
+
+//validation of data 
+//encrypt the password
+
+app.post('/signup', async (req, res) => {
     console.log(req.body)
 
-    const user = new User(req.body)
-    try{
+    
+    try {
+         validateSignUpData(req)
+ 
+         const {firstName, lastName, emailId, password} = req.body
+
+         const passwordHash = await bcrypt.hash(password , 10);
+
+      
+        const user = new User({
+            firstName,
+             lastName, 
+             emailId, 
+             password : passwordHash
+        })
         await user.save();
         res.send("user added successfully")
-    }catch(err){
+    } catch (err) {
         res.status(400).send("error saving the user" + err.message)
     }
 
 })
 
+app.post('/login', async(req, res)=>{
+
+    try{
+       const {emailId, password} = req.body;
+       
+       const user = await User.findOne({emailId : emailId});
+       if(!user){
+        throw new Error("Email is not present in DB")
+       }
+       const isPasswordValid =  await bcrypt.compare(password, user.password);
+
+       if(isPasswordValid){
+         res.send("user login successfull")
+       }else{
+        throw new Error("password is not correct")
+       }
+
+    }catch(err){
+        res.status(400).send("error saving the user" + err.message)
+    }
+})
+
 
 //need to fetch users using email id
 
-app.get('/user', async (req, res)=>{
-    
-    const userEmail = req.body.emailId;
-    
+app.get('/user', async (req, res) => {
 
-    try{
-         const user = await User.find({emailId : userEmail})
-           
-           if(user.length == 0){
+    const userEmail = req.body.emailId;
+
+
+    try {
+        const user = await User.find({ emailId: userEmail })
+
+        if (user.length == 0) {
             res.status(400).send("user not found")
-           }else{
+        } else {
             res.send(user)
-           }
-           
-        
-    }catch(err){
-          res.status(400).send("something went wrong")
+        }
+
+
+    } catch (err) {
+        res.status(400).send("something went wrong")
     }
 })
 
 
 //how to fetch all users fromt the database 
 
-app.get('/fetch', async (req, res)=>{
+app.get('/fetch', async (req, res) => {
 
-    try{
+    try {
 
         const users = await User.find({});
         res.send(users)
 
-    }catch(err){
+    } catch (err) {
         res.status(400).send("something went wrong")
     }
 })
 
 //delete api
 
-app.delete('/user', async(req ,res)=>{
+app.delete('/user', async (req, res) => {
 
-  
 
-    try{
+
+    try {
         const userId = req.body.userId;
         const user = await User.findByIdAndDelete(userId);
         res.send("user deleted successfully")
 
-    }catch(err){
+    } catch (err) {
         res.status(400).send("something went wrong")
     }
-   
+
 })
 
 
 //update api using patch
 
-app.patch('/user', async(req, res)=>{
-
-    
-
-    try{
-
-        const userId = req.body.userId;
-         const data = req.body
-         const user = await User.findByIdAndUpdate({ _id : userId} , data)
-
-         res.send("user updated successfully")
+app.patch('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body
 
 
 
-    }catch(err){
-        res.status(400).send("something went wrong")
+
+    try {
+
+        // const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills", "lastName"];
+
+        // const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+
+        // if (!isUpdateAllowed) {
+        //     throw new Error("update not allowed")
+        // }
+
+        const user = await User.findByIdAndUpdate(userId, data, { new: true });
+        console.log(user)
+
+        res.send("user updated successfully")
+
+
+
+    } catch (err) {
+        res.status(400).send("something went wrong" + err)
     }
 })
 
 
 
-  
-  
 
 
 
-connectDB().then(()=>{
+
+
+connectDB().then(() => {
     console.log("mongodb is connected")
-    app.listen(3001 , ()=>{
+    app.listen(3001, () => {
         console.log("server is running successfully on port 3001...")
     })
-}).catch((err)=>{
+}).catch((err) => {
     console.log("database is not connected")
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
+
+//you need to add validations and unique emailid should be there
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 3,
+        maxLength : 20
     },
 
     lastName : {
@@ -10,20 +15,46 @@ const userSchema = new mongoose.Schema({
     },
 
     emailId : {
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        lowercase: true,
+        trim : true
     },
 
     password : {
         type : String,
+        required : true
     },
 
     age : {
-        type : Number
+        type : Number,
+        min : 18,
     },
 
     gender : {
+        type : String,
+        validate(value){
+            if(!["male", "female", "other"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
+    }, 
+
+    photoUrl : {
         type : String
+    },
+
+    about : {
+        type : String,
+        default : "This is a default about"
+    },
+
+    skills : {
+        type : [String]
     }
+},{
+    timestamps :true
 })
 
 const userModel = mongoose.model("User", userSchema)
