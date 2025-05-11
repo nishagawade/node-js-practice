85bf7680c8c7bc7dfed94b04d6b05964fbcb8d47
nisha <nishagawade2021@gmai.com>
2025-05-11 19:56:40 +0530

    changed login error message

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -11,7 +11,7 @@ const userAuth = async(req, res, next) =>{
         const {token} = req.cookies;
 
         if(!token){
-            throw new Error("token is not valid")
+            return res.status(401).send("Please Login!!")
         }
         const decodedMessage = await jwt.verify(token, "DEV@Tinder$789") 
     
